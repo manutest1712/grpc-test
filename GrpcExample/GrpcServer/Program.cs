7aea929f9e9ceb75eacad7d462d012078ab99ea2
Manu M P <manu.potti@mindcurv.com>
2025-11-26 14:49:07 +0530

    added reflection

--- GrpcExample/GrpcServer/Program.cs
+++ GrpcExample/GrpcServer/Program.cs
@@ -14,8 +14,10 @@ builder.WebHost.ConfigureKestrel(options =>
     });
 });
 
+builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
+app.MapGrpcReflectionService();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
