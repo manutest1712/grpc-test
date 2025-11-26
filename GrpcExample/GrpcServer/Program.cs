4401c1943aa15a2977b84497da4a00fe5f12bbc6
Manu M P <manu.potti@mindcurv.com>
2025-11-26 13:31:14 +0530

    added

--- GrpcExample/GrpcServer/Program.cs
+++ GrpcExample/GrpcServer/Program.cs
@@ -5,16 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
-// Configure Kestrel to listen on a specific port (e.g., 5001)
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(6000); // HTTP/2 over plain TCP (for testing)
-    // For HTTPS with HTTP/2:
-    // options.ListenAnyIP(5001, listenOptions =>
-    // {
-    //     listenOptions.UseHttps();
-    //     listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
-    // });
+    options.ListenAnyIP(6000, listenOptions =>
+    {
+        listenOptions.Protocols =
+            Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
 });
 
 var app = builder.Build();
