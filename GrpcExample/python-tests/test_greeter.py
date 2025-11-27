37fd8dd2c4dfd5d9165c32cf1c214e1066841a78
manutest1712 <manugithub17121980@gmail.com>
2025-11-27 12:01:55 +0530

    added a tc

--- GrpcExample/python-tests/test_greeter.py
+++ GrpcExample/python-tests/test_greeter.py
@@ -1,1 +1,15 @@
+import grpc
+import os
+
+import greet_pb2
+import greet_pb2_grpc
+
+def test_say_hello():
+    server = "localhost:6000"  # hardcoded
+
+    channel = grpc.insecure_channel(server)
+    client = greet_pb2_grpc.GreeterStub(channel)
+
+    response = client.SayHello(greet_pb2.HelloRequest(name="Manu"))
+    assert "Manu" in response.message
 
